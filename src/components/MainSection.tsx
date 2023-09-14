677e54c827b84b2d39a53fcf2e4c0ca96e5fbad4
KASHWAY <targosz.hubert@gmail.com>
2023-09-14 19:02:19 +0200

    Poprawki

--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -10,28 +10,50 @@ const MainSection = () => {
         <Text textAlign={'center'} marginTop={'2vh'} fontSize={'2.5vh'}>
           Generate your ART <br /> by typing in field below any words that comes to your mind!
         </Text>
-        <HStack marginTop={'4vh'} fontSize={'1.8vh'}> 
-          {/* Do zrobienia progress */}
-          <VStack w={'33%'}>  
-              <Progress value={100} w={'100%'} colorScheme='red'></Progress>
-              <Text textAlign={'center'}>Generate Your Image</Text>
-          </VStack>
-          
-         <VStack w={'33%'}>  
-              <Progress value={0} w={'100%'} colorScheme='red'></Progress>
-              <Text textAlign={'center'}>Choose Your Product</Text>
-          </VStack>
-
-          <VStack w={'33%'}>  
-              <Progress value={0} w={'100%'} colorScheme='red'></Progress>
-              <Text textAlign={'center'}>Order Your Product</Text>
-          </VStack>
-
+        <HStack spacing={0} marginTop="4vh">
+          <Box
+            bg="#CB2B28" 
+            h="8px"
+            flex="1" 
+            borderRadius={'full'}
+            display={'flex'}
+            alignItems={'center'}
+            justifyContent={'center'}
+          >
+            <Img src={`${IMG_PATH}ikona1.svg`} alt='ikona 1 czerwona'/>
+          </Box>
+          <Box
+            bg="#E5E5E7" 
+            h="8px" 
+            flex="1" 
+            display={'flex'}
+            alignItems={'center'}
+            justifyContent={'center'}
+          > 
+            <Img src={`${IMG_PATH}ikona2.svg`} alt='ikona 2 szara'/>
+          </Box>
+          <Box
+            bg="#E5E5E7" 
+            h="8px" 
+            flex="1" 
+            borderRadius={'full'}
+            display={'flex'}
+            alignItems={'center'}
+            justifyContent={'center'}
+          >
+            <Img src={`${IMG_PATH}ikona3.svg`} alt='ikona 3 szara'/>
+          </Box>
         </HStack>
 
+        <Flex justifyContent="space-between" width="100%" marginTop={'2vh'} fontFamily={'Raleway Bold'}>
+            <Text flex="1" textAlign="center" color={'#CE2B28'}>Generate <br/> Image</Text>
+            <Text flex="1" textAlign="center">Choose Your Product</Text>
+            <Text flex="1" textAlign="center">Order</Text>
+        </Flex>
+      
         <Flex marginTop={'4vh'} boxShadow="lg" h={'6vh'}><Input placeholder='What do you want to create?' type='text' bg={'#B7B2B25C'} opacity={'36%'} h={'100%'}/><Img src={`${IMG_PATH}rediconpen.svg`} alt='red icon pen' height={'100%'} boxShadow="2xl"/></Flex>
 
-        <HStack w={'100%'} h={'6vh'} marginTop={'2vh'} fontSize={'1.8vh'} fontWeight={'600'}>
+        <HStack w={'100%'} h={'6vh'} marginTop={'2vh'} fontSize={'1.8vh'} fontWeight={'600'} fontFamily={'Raleway Regular'}>
           <Flex justifyContent={'center'} alignItems={'center'} textAlign={'center'} w={'33%'} h={'100%'} bg={'#F5F5F6'} borderRadius={'lg'}>High Quality</Flex>
           <Flex justifyContent={'center'} alignItems={'center'} textAlign={'center'} w={'33%'} h={'100%'} bg={'#F5F5F6'} borderRadius={'lg'}>Pop Art</Flex>
           <Flex justifyContent={'center'} alignItems={'center'} textAlign={'center'} w={'34%'} h={'100%'} bg={'#F5F5F6'} borderRadius={'lg'}>Realistic Look</Flex>
@@ -42,7 +64,7 @@ const MainSection = () => {
             <Box w={'10%'}><Img src={`${IMG_PATH}leftarrow-icon.svg`} alt='left arrow icon'/></Box>
             <VStack w={'90%'}>
                 
-                <Text>Style Preview</Text>
+                <Text fontFamily={'Raleway Regular'}>Style Preview</Text>
                 <Img src={`${IMG_PATH}hero-icon.svg`} alt='hero icon gray'/>
             </VStack>            
           </Flex>
@@ -65,9 +87,9 @@ const MainSection = () => {
                 <HStack><Text fontSize={'2vh'} textAlign={'center'}>See More</Text><Img src={`${IMG_PATH}arrow-icon.svg`} alt='arrow icon'/></HStack>
                 <Img src={`${IMG_PATH}demon-icon.svg`} alt='demon icon'/>
                 <Box w={'100%'} h={'5vh'} borderRadius={'lg'}>
-                  <Flex w={'100%'} bg={'#c4c4c4'} h={'100%'} borderRadius={'lg'} fontFamily={'Open Sans Light'}>
+                  <Flex w={'100%'} bg={'#E5E5E7'} h={'100%'} borderRadius={'lg'} fontFamily={'Raleway Regular'}>
                       <HStack w={'70%'} h={'100%'} paddingLeft={'2vh'}><Text>punk heart, gothic</Text></HStack>
-                      <Flex w={'30%'} h={'100%'} bg={'#a4a4a4'} justifyContent={'center'} alignItems={'center'} textAlign={'center'}><Text>ARTISTIC</Text></Flex>
+                      <Flex w={'30%'} h={'100%'} bg={'#CFCFD0'} justifyContent={'center'} alignItems={'center'} textAlign={'center'}><Text>ARTISTIC</Text></Flex>
                   </Flex>
                   
                 </Box>
